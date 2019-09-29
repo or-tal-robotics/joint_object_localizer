b5bb87141ed6490c326cfc36b250961bea2e3b5d
elbazam <aoosha@gmail.com>
2019-09-29 13:32:38 +0300

    Changing the code that the subscriber will be able to read the latest data and not the old one as it did before.

--- scripts/Object_Detection.py
+++ scripts/Object_Detection.py
@@ -28,188 +28,9 @@ def Robot_callback(data):
 
 def SSD_callback(data):
         
-        global SSD_info
-        global circle
-        global scan_point
-        global Robot_Pose
-
-        SSD_info = data
-
-        angle_jumps = scan_point.angle_increment
-        phi_max = len(scan_point.ranges) * angle_jumps
-        theta_camera_max = 1.4
-
-        # The angle that needed to be added to the camera angle to get the angle for the sensor:
-        correction_for_sensor = (phi_max + theta_camera_max) / 2
-        size =  (len(scan_point.ranges) - 1)/2
-        Theta_list = OG_List()
+    global SSD_info
+    SSD_info = data
         
-        # In case there is no object that has been detected:
-        
-        
-        for ii in range (0,len(SSD_info.outputs)):
-            Theta_Object = Object_Geometry()
-            # Getting the info from the camera:
-            x_min_new = SSD_info.outputs[ii].x_min 
-            x_max_new = SSD_info.outputs[ii].x_max
-            cls_num = SSD_info.outputs[ii].cls
-            
-            Theta_Object.probabilities = SSD_info.outputs[ii].probability_distribution
-            # Theta from camera:
-            theta_min = x_min_new*(1.4/300)
-            theta_max = x_max_new*(1.4/300)
-            # Angle from the sensor:
-            angle_max = int((correction_for_sensor - theta_min) / angle_jumps)
-            angle_min = int((correction_for_sensor - theta_max) / angle_jumps)
-
-            # Orientation of the robot:
-            R_qx = Robot_Pose.pose.orientation.x
-            R_qy = Robot_Pose.pose.orientation.y
-            R_qz = Robot_Pose.pose.orientation.z
-            R_qw = Robot_Pose.pose.orientation.w
-            [yaw, pitch, roll] = quaternion_to_euler(R_qx,R_qy,R_qz,R_qw)
-            
-            # Location of the robot: 
-            x_R = Robot_Pose.pose.position.x
-            y_R = Robot_Pose.pose.position.y
-
-            las_points = np.zeros((2*size + 1,2))
-            # Initializing the array of the points:
-            circle = []
-
-            R = np.array(scan_point.ranges)
-            # Initializing an inf values:
-            R[np.isinf(R)] = 0
-
-            for i in range (0,size):
-                
-                if R[i] == 0:
-                    las_points[i,0] = 100000
-                    las_points[i,1] = 100000
-                    continue
-                las_points[i,0] = -R[i] * np.cos((size - i) * angle_jumps - np.pi)
-                las_points[i,1] = R[i] * np.sin((size - i) * angle_jumps - np.pi)
-                
-            for i in range (size,2*size+1):
-                if R[i] == 0:
-                    las_points[i,0] = 100000
-                    las_points[i,1] = 100000
-                    continue
-                las_points[i,0] = R[i] * np.cos((i-size) * angle_jumps)
-                las_points[i,1] = R[i] * np.sin((i-size) * angle_jumps)
-            
-            # Round objects:
-            if cls_num == 5:
-                
-                # Initializing the circle array:
-                circle = np.array((las_points[angle_min:angle_max,:]))
-
-                # Bounds for DE algoritm
-                bounds = [ [np.amin(circle[:,0])-0.1 , np.amin([np.amax(circle[:,0])+0.1 , 2.5])] , \
-                    [np.amin(circle[:,1])-0.1,np.amin([np.amax(circle[:,1])+0.1 , 2.5])],[0.02,0.08]]
-                
-                # DE algoritm
-                circle_minimized_values = differential_evolution(probability_for_can,bounds \
-                    , maxiter = 100,  popsize=15, tol=0.00001)
-
-                v0 = New_Ce_array(circle_minimized_values.x[0],circle_minimized_values.x[1],x_R,y_R,yaw)
-                
-                Theta_Object.x_center = v0[0]
-                Theta_Object.y_center = v0[1]
-                Theta_Object.r = circle_minimized_values.x[2]
-                Theta_Object.cls = cls_num
-                
-                Theta_list.object_list.append(Theta_Object)
-                Theta_Publisher.publish(Theta_Object)
-                continue
-            # Box:
-            elif cls_num == 18 or cls_num == 20:
-
-                # Initializing the box array:
-                box = np.array((las_points[angle_min:angle_max,:]))
-                # Bounds for DE algoritm
-                bounds_R = [ [np.amin(box[:,0])-0.1 , np.amin([np.amax(box[:,0])+0.1 , 3.5])] , \
-                    [np.amin(box[:,1])-0.1,np.amin([np.amax(box[:,1])+0.1 , 3.5])],[-np.pi,np.pi],[0.2,0.8],[0.2,0.8]]
-
-                nbrs_obj = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(box)
-                # For being able to send two values for DE algoritm:
-                iterable_function = lambda x: Rectangle_Likelihood(x,nbrs_obj)
-                # DE angoritm:
-                rectangle_minimized_values = differential_evolution(iterable_function,bounds_R \
-                    , maxiter = 100,  popsize=15, tol=0.00001)
-                
-                Theta_Object.x_center , Theta_Object.y_center = New_Ce_array(rectangle_minimized_values.x[0] ,rectangle_minimized_values.x[1],x_R,y_R,yaw)
-                Theta_Object.a = rectangle_minimized_values.x[3]
-                Theta_Object.b = rectangle_minimized_values.x[4]
-                Theta_Object.angle = rectangle_minimized_values.x[2] + yaw
-                Theta_Object.cls = cls_num
-                Theta_Publisher.publish(Theta_Object)
-                Theta_list.object_list.append(Theta_Object)
-                continue
-                
-            # cat,dog,horse:
-            elif cls_num == 8 or cls_num == 12 or cls_num == 13:
-        
-                # Initializing the box array:
-                ellipse = np.array((las_points[angle_min:angle_max,:]))
-                # Bounds for DE algoritm
-                bounds_E = [ [np.amin(ellipse[:,0])-0.1 , np.amin([np.amax(ellipse[:,0])+0.1 , 3.5])] , \
-                    [np.amin(ellipse[:,1])-0.1,np.amin([np.amax(ellipse[:,1])+0.1 , 3.5])],[-np.pi,np.pi],[0.1,0.8],[0.1,0.8]]
-
-                nbrs_obj = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(ellipse)
-                # For being able to send two values for DE algoritm:
-                iterable_function = lambda x: Ellipse_likelihood(x,nbrs_obj)
-                # DE angoritm:
-                elliptical_minimized_values = differential_evolution(iterable_function,bounds_E \
-                    , maxiter = 100,  popsize=15, tol=0.00001)
-                
-                Theta_Object.x_center , Theta_Object.y_center = New_Ce_array(elliptical_minimized_values.x[0] ,elliptical_minimized_values.x[1],x_R,y_R,yaw)
-                Theta_Object.a = elliptical_minimized_values.x[3]
-                Theta_Object.b = elliptical_minimized_values.x[4]
-                Theta_Object.angle = elliptical_minimized_values.x[2] + yaw
-                Theta_Object.cls = cls_num
-                #Theta_Publisher.publish(Theta_Object)
-                Theta_list.object_list.append(Theta_Object)
-                '''
-                # -----------------------Printing test----------------:
-                cat = Marker()
-                # Test for putting a can in the map.
-                cat.header.frame_id = 'map'
-                
-                # Colore of can
-                cat.color.r = 100
-                cat.color.g = 0
-                cat.color.b = 20
-                cat.color.a = 1
-                # Rest of things:
-                cat.id = 1
-                cat.type = 3
-                cat.action = 0
-                cat.lifetime.secs = 0
-
-                # Update the new center:
-                cat.pose.position.x = Theta_Object.x_center
-                cat.pose.position.y = Theta_Object.y_center
-                cat.pose.position.z = 0.15
-                # Size of the can
-                cat.scale.x = 2 * Theta_Object.a
-                cat.scale.y = 2 * Theta_Object.b
-                cat.scale.z = 0.3
-
-                [x_b,y_b,z_b,w_b] = quaternion_from_euler(0,0,Theta_Object.angle)
-                # Orientation of the can
-                cat.pose.orientation.x = x_b
-                cat.pose.orientation.y = y_b
-                cat.pose.orientation.z = z_b
-                cat.pose.orientation.w = w_b
-
-                cat.ns = 'cat'
-                cat.header.stamp = rospy.Time.now()
-
-                cat_pub.publish(cat)
-                '''
-                continue
-        Theta_list_pub.publish(Theta_list)
 
 
 
@@ -249,21 +70,213 @@ scan_point_sub = rospy.Subscriber('/scan' ,LaserScan , callback_laser )
 rospy.wait_for_message('/scan',LaserScan)
 Robot_sub = rospy.Subscriber('/slam_out_pose',PoseStamped,Robot_callback)
 rospy.wait_for_message('/slam_out_pose',PoseStamped)
+SSD_sub = rospy.Subscriber('/im_info',SSD_Outputs,SSD_callback,queue_size=1)
+rospy.wait_for_message('/im_info',SSD_Outputs)
 
 while not rospy.is_shutdown():
-    SSD_sub = rospy.Subscriber('/im_info',SSD_Outputs,SSD_callback)
-    rospy.wait_for_message('/im_info',SSD_Outputs)
+    
+    if SSD_info.outputs[0].cls == -1:
+        Theta_list = OG_List()
+        Theta_list.object_list = []
+        Theta_list_pub.publish(Theta_list)
+        continue
+    Theta_list = OG_List()
+    # globals
+    Theta_list.object_list = []
+    # Calculating the maximum angle of the sensor:
+    angle_jumps = scan_point.angle_increment
+    phi_max = len(scan_point.ranges) * angle_jumps
+    theta_camera_max = 1.4
 
+    # The angle that needed to be added to the camera angle to get the angle for the sensor:
+    correction_for_sensor = (phi_max + theta_camera_max) / 2
+    size =  (len(scan_point.ranges) - 1)/2
+    
+    
+    # In case there is no object that has been detected:
+    
+    
+    for ii in range (0,len(SSD_info.outputs)):
+        Theta_Object = Object_Geometry()
+        # Getting the info from the camera:
+        x_min_new = SSD_info.outputs[ii].x_min 
+        x_max_new = SSD_info.outputs[ii].x_max
+        cls_num = SSD_info.outputs[ii].cls
+
+        
+        # Theta from camera:
+        theta_min = x_min_new*(1.4/300)
+        theta_max = x_max_new*(1.4/300)
+        # Angle from the sensor:
+        angle_max = int((correction_for_sensor - theta_min) / angle_jumps)
+        angle_min = int((correction_for_sensor - theta_max) / angle_jumps)
+
+        # Orientation of the robot:
+        R_qx = Robot_Pose.pose.orientation.x
+        R_qy = Robot_Pose.pose.orientation.y
+        R_qz = Robot_Pose.pose.orientation.z
+        R_qw = Robot_Pose.pose.orientation.w
+        [yaw, pitch, roll] = quaternion_to_euler(R_qx,R_qy,R_qz,R_qw)
+        
+        # Location of the robot: 
+        x_R = Robot_Pose.pose.position.x
+        y_R = Robot_Pose.pose.position.y
+
+        las_points = np.zeros((2*size + 1,2))
+        # Initializing the array of the points:
+        circle = []
+
+        R = np.array(scan_point.ranges)
+        # Initializing an inf values:
+        R[np.isinf(R)] = 0
+
+        for i in range (0,size):
+            
+            if R[i] == 0:
+                las_points[i,0] = 100000
+                las_points[i,1] = 100000
+                continue
+            las_points[i,0] = -R[i] * np.cos((size - i) * angle_jumps - np.pi)
+            las_points[i,1] = R[i] * np.sin((size - i) * angle_jumps - np.pi)
+            
+        for i in range (size,2*size+1):
+            if R[i] == 0:
+                las_points[i,0] = 100000
+                las_points[i,1] = 100000
+                continue
+            las_points[i,0] = R[i] * np.cos((i-size) * angle_jumps)
+            las_points[i,1] = R[i] * np.sin((i-size) * angle_jumps)
+        
+        # Round objects:
+        if cls_num == 5:
+            
+            
+            # Initializing the circle array:
+            circle = np.array((las_points[angle_min:angle_max,:]))
+
+            # Bounds for DE algoritm
+            bounds = [ [np.amin(circle[:,0])-0.1 , np.amin([np.amax(circle[:,0])+0.1 , 2.5])] , \
+                [np.amin(circle[:,1])-0.1,np.amin([np.amax(circle[:,1])+0.1 , 2.5])],[0.02,0.08]]
+            
+            # DE algoritm
+            circle_minimized_values = differential_evolution(probability_for_can,bounds \
+                , maxiter = 100,  popsize=15, tol=0.00001)
+
+            v0 = New_Ce_array(circle_minimized_values.x[0],circle_minimized_values.x[1],x_R,y_R,yaw)
+            Theta_Object.probabilities = SSD_info.outputs[ii].probability_distribution
+            Theta_Object.x_center = v0[0]
+            Theta_Object.y_center = v0[1]
+            Theta_Object.r = circle_minimized_values.x[2]
+            Theta_Object.a = 0
+            Theta_Object.b = 0
+            Theta_Object.angle = 0
+
+            Theta_Object.cls = cls_num
+            
+            Theta_list.object_list.append(Theta_Object)
+            #Theta_Publisher.publish(Theta_Object)
+            
+        # Box:
+        elif cls_num == 18 or cls_num == 20:
+
+            
+            # Initializing the box array:
+            box = np.array((las_points[angle_min:angle_max,:]))
+            # Bounds for DE algoritm
+            bounds_R = [ [np.amin(box[:,0])-0.1 , np.amin([np.amax(box[:,0])+0.1 , 3.5])] , \
+                [np.amin(box[:,1])-0.1,np.amin([np.amax(box[:,1])+0.1 , 3.5])],[-np.pi,np.pi],[0.2,0.8],[0.2,0.8]]
+
+            nbrs_obj = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(box)
+            # For being able to send two values for DE algoritm:
+            iterable_function = lambda x: Rectangle_Likelihood(x,nbrs_obj)
+            # DE angoritm:
+            rectangle_minimized_values = differential_evolution(iterable_function,bounds_R \
+                , maxiter = 100,  popsize=15, tol=0.00001)
+            Theta_Object.probabilities = SSD_info.outputs[ii].probability_distribution
+            Theta_Object.x_center , Theta_Object.y_center = New_Ce_array(rectangle_minimized_values.x[0] ,rectangle_minimized_values.x[1],x_R,y_R,yaw)
+            Theta_Object.a = rectangle_minimized_values.x[3]
+            Theta_Object.b = rectangle_minimized_values.x[4]
+            Theta_Object.angle = rectangle_minimized_values.x[2] + yaw
+            Theta_Object.r = 0
+            Theta_Object.cls = cls_num
+            
+            Theta_list.object_list.append(Theta_Object)
+            #Theta_Publisher.publish(Theta_Object)
+            
+        # cat,dog,horse:
+        elif cls_num == 8 or cls_num == 12 or cls_num == 13:
+            
+            # Initializing the box array:
+            ellipse = np.array((las_points[angle_min:angle_max,:]))
+            # Bounds for DE algoritm
+            bounds_E = [ [np.amin(ellipse[:,0])-0.1 , np.amin([np.amax(ellipse[:,0])+0.1 , 3.5])] , \
+                [np.amin(ellipse[:,1])-0.1,np.amin([np.amax(ellipse[:,1])+0.1 , 3.5])],[-np.pi,np.pi],[0.1,0.8],[0.1,0.8]]
+
+            nbrs_obj = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(ellipse)
+            # For being able to send two values for DE algoritm:
+            iterable_function = lambda x: Ellipse_likelihood(x,nbrs_obj)
+            # DE angoritm:
+            elliptical_minimized_values = differential_evolution(iterable_function,bounds_E \
+                , maxiter = 100,  popsize=15, tol=0.00001)
+            
+            Theta_Object.probabilities = SSD_info.outputs[ii].probability_distribution
+            Theta_Object.x_center , Theta_Object.y_center = New_Ce_array(elliptical_minimized_values.x[0] ,elliptical_minimized_values.x[1],x_R,y_R,yaw)
+            Theta_Object.a = elliptical_minimized_values.x[3]
+            Theta_Object.b = elliptical_minimized_values.x[4]
+            Theta_Object.angle = elliptical_minimized_values.x[2] + yaw
+            Theta_Object.r = 0
+            Theta_Object.cls = cls_num
+            #Theta_Publisher.publish(Theta_Object)
+            Theta_list.object_list.append(Theta_Object)
+            
+            '''
+            # -----------------------Printing test----------------:
+            cat = Marker()
+            # Test for putting a can in the map.
+            cat.header.frame_id = 'map'
+            
+            # Colore of can
+            cat.color.r = 100
+            cat.color.g = 0
+            cat.color.b = 20
+            cat.color.a = 1
+            # Rest of things:
+            cat.id = 1
+            cat.type = 3
+            cat.action = 0
+            cat.lifetime.secs = 0
+
+            # Update the new center:
+            cat.pose.position.x = Theta_Object.x_center
+            cat.pose.position.y = Theta_Object.y_center
+            cat.pose.position.z = 0.15
+            # Size of the can
+            cat.scale.x = 2 * Theta_Object.a
+            cat.scale.y = 2 * Theta_Object.b
+            cat.scale.z = 0.3
+
+            [x_b,y_b,z_b,w_b] = quaternion_from_euler(0,0,Theta_Object.angle)
+            # Orientation of the can
+            cat.pose.orientation.x = x_b
+            cat.pose.orientation.y = y_b
+            cat.pose.orientation.z = z_b
+            cat.pose.orientation.w = w_b
+
+            cat.ns = 'cat'
+            cat.header.stamp = rospy.Time.now()
+
+            cat_pub.publish(cat)
+            '''
+            
+    Theta_list_pub.publish(Theta_list)
 
 
 # Test:
 ##cat_pub = rospy.Publisher('/Cat' , Marker , queue_size = 5)
 
-# globals
 
 
 
-# ------------------------------Begining of code-------------------------------
-# Calculating the maximum angle of the sensor:
+
 
     
